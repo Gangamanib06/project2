571d17f970077f3acf7ae7c352bad01ad241b0f3
macbookpro <macbookpro@192.168.0.41>
2023-10-09 14:42:07 +0530

    First commit

---
+++ corejava1/src/com/tnisf/Encapsulation/Encapsulation.java
@@ -0,0 +1,15 @@
+package com.tnisf.Encapsulation;
+
+public class Encapsulation {
+
+	public static void main(String[] args) {
+		public int serialno;
+		public String name;
+		public int age;
+		
+		//memberfunction
+		
+		public void show()
+	   System.out.println("serialno"+ serialno+ "\t"+ "name"+ name+ "/t"+ "age"+age);
+	}
+}
