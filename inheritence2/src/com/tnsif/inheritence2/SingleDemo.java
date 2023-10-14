c234642902905f464ae34ffa02b05b1a40fe645b
macbookpro <macbookpro@192.168.254.1>
2023-10-14 20:05:36 +0530

    first commit

---
+++ inheritence2/src/com/tnsif/inheritence2/SingleDemo.java
@@ -0,0 +1,9 @@
+package com.tnsif.inheritence2;
+
+public class SingleDemo {
+}
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student st = new Student("Anusha",343526120,"Hyderabad",9089079);
+		System.out.println(st);
+	
---
+++ inheritence2/src/com/tnsif/inheritence2/StudentextendsCitizen.java
@@ -0,0 +1,27 @@
+package com.tnsif.inheritence2;
+public class StudentextendsCitizen {
+      private int rollno;
+      private String collegename;
+     //default constructor 
+	public Student() {
+		super();
+	}
+	
+	 
+	public Student(String name, long aadharno, String address, long phno) {
+		super(name, aadharno, address, phno);//accessing from the parent class constructor
+		// TODO Auto-generated constructor stub
+		this.rollno = rollno;
+		this.collegename= collegename;
+	}
+	public Student(int rollno, String collegename) {
+		super();
+			this.rollno = rollno;
+			this.collegename = collegename;
+		}
+
+
+	//getters and setters
+	public int getRollno() {
+		return rollno;
+	}
