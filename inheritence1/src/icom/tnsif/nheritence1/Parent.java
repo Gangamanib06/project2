f9d72b6611b94466c16e67e1c1d2ad2054f1720a
macbookpro <macbookpro@192.168.254.1>
2023-10-14 19:47:44 +0530

    first commit

---
+++ inheritence1/src/icom/tnsif/nheritence1/Parent.java
@@ -0,0 +1,20 @@
+package icom.tnsif.nheritence1;
+public class Parent {//base class 
+	 int var=100;
+}
+public class Superdemo extends Parent {
+
+	int var=50;
+	void display() {
+		System.out.println("the child value is "+var);
+		System.out.println("the child value is" +super.var);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Superdemo ob= new Superdemo();
+		ob.display();
+
+	}
+
+}
